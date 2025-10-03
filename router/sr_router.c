b5547b5c978c319b47dde9b4c0789ce69efe7ee5
minha-faheem <minha.faheem@hotmail.com>
2025-10-03 20:58:30 +0000

    ARP reply/request, start IP packets

--- router/sr_router.c
+++ router/sr_router.c
@@ -61,79 +61,104 @@ void sr_handlepacket(struct sr_instance *sr, uint8_t *packet /* lent */,
 
   printf("*** -> Received packet of length %d \n", len);
 
-  /* fill in code here */
+  /* CODE HERE */
+  /* Need to check if packet is (1) IP packet or (2) ARP reply/request, and handle accordingly. */
 
-  /* Need to check if packet is (1) IP packet or (2) ARP reply/request */
 
-  /* Print ethernet header */
-  print_hdr_eth(packet);
+  uint8_t *copy = malloc(len);            /* Make copy of packet */
+  memcpy(copy, packet, len);
 
-  uint16_t ethtype = ethertype(packet);   /* Determine Ethernet type */
+  print_hdr_eth(copy);                    /* Print Ethernet header */
+  uint16_t ethtype = ethertype(copy);   /* Determine Ethernet type */
 
-  if (ethtype == ethertype_ip) {
+  if (ethtype == ethertype_ip) {          /* If packet is IP packet */
     printf(">>> IP Packet Received:\n");
-    print_hdr_ip(packet + sizeof(sr_ethernet_hdr_t));     /* Print IP header */
-    /* TODO: handle IP packets */
+    print_hdr_ip(copy + sizeof(sr_ethernet_hdr_t));
+
+    /* Is the packet destined to this router? Aka me? Is the destination IP the same as my IP?
+    If so, ICMP echo request. TCP/UDP */
+
+
+    /* 1. Sanity-check the packet (meets minimum length and has correct checksum). 
+    If a packet is malformed, the router should silently drop it. */
+    sr_ip_hdr_t *ip_header = (sr_ip_hdr_t *)(copy + sizeof(sr_ethernet_hdr_t)); /* Cast ip header to retrieve destination IP address */
+    uint16_t ip_header_length = ip_header->ip_hl * 4; /* Calculate IP header length */
+    if (len < sizeof(sr_ethernet_hdr_t) + ip_header_length) {
+      free(copy);
+      return;
+    }
+    uint16_t checksum = ip_header->ip_sum; /* Store original checksum */
+    ip_header->ip_sum = 0;                 /* Set checksum to 0 for calculation */
+    if (cksum(ip_header, ip_header_length) != checksum) {
+      free(copy);
+      return; /* Drop packet */
+    }
+    ip_header->ip_sum = checksum;          /* Restore original checksum */
+
+    /* 2. Decrement the TTL by 1, and recompute the packet checksum over the modified header. */
+    ip_header->ip_ttl -= 1;
+
+    /* 3. Find out which entry in the routing table has the longest prefix match with the destination IP address. */ 
+    struct sr_rt *routing_table = sr->routing_table; /* Get routing table */
+    struct sr_rt *longest_match = NULL;               /* Initialize longest match */
+    uint32_t dest_ip = ip_header->ip_dst;             /* Get destination IP address */
+    while (routing_table != NULL) {
+      if ((dest_ip & routing_table->mask.s_addr) == (routing_table->dest.s_addr & routing_table->mask.s_addr)) {
+        if (longest_match == NULL || ntohl(routing_table->mask.s_addr) > ntohl(longest_match->mask.s_addr)) {
+          longest_match = routing_table; /* Update longest match */
+        }
+      }
+      routing_table = routing_table->next; /* Move to next entry */
+    }
+
+    /* 4. Check the ARP cache for the next-hop MAC address corresponding to the next-hop IP.
+    If it’s there, send it. Otherwise, send an ARP request for the next-hop IP (if one hasn’t
+    been sent within the last second), and add the packet to the queue of packets waiting on
+    this ARP request.*/
+
+
+
+
+
+
+
 
   }
   
-  else if (ethtype == ethertype_arp) {
+  else if (ethtype == ethertype_arp) {                                /* If packet is ARP reply/request */
     printf(">>> ARP Packet Received:\n");
-    print_hdr_arp(packet + sizeof(sr_ethernet_hdr_t));        /* Print ARP header */
-    sr_arp_hdr_t *arp_hdr = (struct sr_arp_hdr_t *)(packet);        /* Cast arp header */
+    print_hdr_arp(copy + sizeof(sr_ethernet_hdr_t));                 /* Print ARP header */
+    
+    sr_arp_hdr_t *arp_header = (struct sr_arp_hdr_t *)(copy);        /* Cast arp header */
 
     /* Check if ARP reply/request */
-    if (arp_hdr->ar_op == arp_op_request) {
+    if (arp_header->ar_op == arp_op_request) {
       struct sr_if *interface = sr->if_list;
       while (interface != NULL) {
-          if (interface->ip == arp_hdr->ar_tip) { // Compare target IP with the interface's IP
-              /* Since a match is found, I send it as an ARP reply */
-              handle_arpreq()
-              break; // Exit the loop if a match is found
+          if (interface->ip == arp_header->ar_tip) {                          /* Compare target IP with the router's interface IPs */
+            /* Since a match is found, send it over as an ARP reply */
+            arp_header->ar_op = arp_op_reply;                                 /* Repackage packet as ARP Reply */
+            sr_send_packet(sr, (uint8_t *)arp_header, len, interface->name);  /* Send packet */
+            break;                                                            /* Exit the loop if a match is found */
           }
-          interface = interface->next; // Move to the next interface
-      }
-      /* TODO: figure out what to do if no router match found */
-    }
-    else if (arp_hdr->ar_op == arp_op_reply) {
-      /* The ARP reply processing code should move entries from the ARP request
-      queue to the ARP cache:
-
-      # When servicing an arp reply that gives us an IP->MAC mapping
-      req = arpcache_insert(ip, mac)
-
-      if req:
-          send all packets on the req->packets linked list
-          arpreq_destroy(req) */
-
-      /* CACHE -> sr_instance->cache : need address of the cache to match the caches */
-      /* if not NULL, (found pointer to sr_arpreq qith IP), inserts IP to MAC mapping in cache */
-      /* if NULL, assume we're dropping it from cache */
-      /* First, look for MAC address (need it for arpcache_insert)*/
-
-    // assume router IP = mac address
-
-    
-
-
-
-      /* store the interface whose ip address matches it, grab its mac address */
-      /* MAKE helper function that looks for a matching interface from the list, use it here */
-      if (sr_arpcache_lookup->mac) {
-        struct sr_arpreq *sr_arpcache_insert = sr_arpcache_insert(sr->cache, if->addr, arp_hdr->ar_tip);
-        if (sr_arpcache_insert) <- this is a request btw {
-          for (each packet in sr_arpcache_insert->packets) {
-            sr_send_packet(packet);
-          }
-          arpreq_destroy(sr_arpcache_insert)
-        }
+          interface = interface->next;
       }
 
-
-    }
-
+    } else if (arp_header->ar_op == arp_op_reply) {
+      struct sr_if *interface = sr_get_interface(sr, interface);              /* Get the interface instance */
+      struct sr_arpreq *req = sr_arpcache_insert(sr->cache, interface->addr, arp_header->ar_tip);   /* Inserts this IP to MAC mapping in the cache, if found */
+      if (req == NULL) {
+        free(copy);
+        return;
+      }
+      struct sr_packet *req_packet = req->packets;                          /* Get the list of packets waiting on this ARP request */
+      while (req_packet != NULL) {
+        struct sr_packet *next_req_packet = req_packet->next;               /* Store next packet before sending current packet */
+        sr_send_packet(sr, req_packet, req_packet->len, req_packet->iface); /* Send the packet */
+        req_packet = next_req_packet;                                       /* Move to the next packet */
+      }
   }
-
+  free(copy);
 } /* end sr_ForwardPacket */
 
 
